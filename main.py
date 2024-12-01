afc211354f0f25625e8ad58d62418715ee1e0341
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-12-01 09:23:41 +0100

    main

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
+from slowapi.errors import RateLimitExceeded
 
-from src.api import contacts, utils
+from logger import logger
+from src.api import auth, contacts, users, utils
 
 app = FastAPI()
 
+
+origins = ["http://localhost:8000"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    logger.warning(f"Rate limit exceeded for '{request.client.host}' host.")
+    return JSONResponse(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        content={"error": "Перевищено ліміт запитів. Спробуйте пізніше."},
+    )
+
+
+app.include_router(auth.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
 app.include_router(utils.router, prefix="/api")
 
 
