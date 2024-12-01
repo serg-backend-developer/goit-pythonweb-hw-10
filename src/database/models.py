5e1b94f1fd71b75a65af8572df9a0d62fbe2a3e2
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-12-01 09:38:06 +0100

    user model

--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime, date
-from sqlalchemy import Integer, String, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.sql.sqltypes import Date, DateTime
+from sqlalchemy import Integer, String, func, Column, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.sql.sqltypes import Boolean, Date, DateTime
 
 
 class Base(DeclarativeBase):
@@ -24,3 +24,19 @@ class Contact(Base):
     updated_at: Mapped[datetime] = mapped_column(
         "updated_at", DateTime, default=func.now(), onupdate=func.now()
     )
+    user_id = Column(
+        "user_id", ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    user = relationship("User", backref="contacts")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    avatar = Column(String(255), nullable=True)
+    is_confirmed = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=func.now())
