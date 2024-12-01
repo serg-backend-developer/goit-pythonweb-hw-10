ce7f4f3858a6dbc1e6686352e4000dd2ac8243cf
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-12-01 09:38:34 +0100

    user contacts

--- src/schemas/contacts.py
+++ src/schemas/contacts.py
@@ -17,3 +17,27 @@ class ContactResponseModel(ContactModel):
     created_at: datetime
     updated_at: Optional[datetime]
     model_config = ConfigDict(from_attributes=True)
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    avatar: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class RequestEmail(BaseModel):
+    email: EmailStr
